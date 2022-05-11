9cbd32a54ae7a3d90be457a0b1dce1dced2983a6
Konstantin <kostakv@outlook.com>
2022-05-11 22:50:44 +0000

    Added basic security to app, for users and non users

--- express_server.js
+++ express_server.js
@@ -41,13 +41,40 @@ const users = {
     email: "Kostakv@outlook.com", 
     password: "password"
   }
+   
 }
 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+        longURL: "https://www.tsn.ca",
+        userID: "master"
+    },
+    i3BoGr: {
+        longURL: "https://www.google.ca",
+        userID: "aJ48lW"
+    }
 };
+
+function checkURL(url){
+  for (const key in urlDatabase) {
+    if (url === key){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+function urlsForUser(id, url){
+  for (const key in urlDatabase) {
+    if (id === urlDatabase[key].userID && url === key){
+      return true;
+    }
+  }
+  return false;
+}
+
 
 function checkEmail(email){
   
@@ -59,7 +86,7 @@ function checkEmail(email){
   return false; 
 }
 
-function checkPassword(password, email){
+function checkPassword(password){
   for (const user in users){
     if (users[user].password == password){
       return true;
@@ -72,7 +99,11 @@ function checkPassword(password, email){
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], users: users, userID: req.cookies["userID"] };
+  if (req.cookies["userID"] === undefined){
+    res.redirect("/login")
+  } else {
   res.render("urls_new",templateVars);
+  }
   
 });
 
@@ -117,41 +148,76 @@ app.get("/set", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  if (req.cookies["userID"] === undefined){
+    res.status(400).send('Error, access denied!')
+  } else {
   console.log(req.body); 
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
-  res.redirect("/urls");         
+  urlDatabase[shortURL] = {longURL: longURL, userID: req.cookies["userID"]};
+  console.log(urlDatabase)
+  //urlDatabase[shortURL] = longURL;
+  res.redirect("/urls"); 
+  }        
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL, users: users, userID: req.cookies["userID"] };
+  let access = true;
+  if (checkURL(req.params.shortURL)){
+    if (urlsForUser(req.cookies["userID"],req.params.shortURL)){
+      access = true;
+    }else {
+      access = false;
+    }
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL, users: users, userID: req.cookies["userID"], urls: urlDatabase, access: access };
   res.render("urls_show", templateVars);
+    
+  } else {
+    res.status(400).send("Short URL does not exist!")
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+  let error = true;
+  for (const key in urlDatabase) {
+    if (req.params.shortURL === key){
+        error = false;
+        const longURL = urlDatabase[req.params.shortURL].longURL
+        res.redirect(longURL);
+    }
+  }
+  if (error){
+    res.status(400).send('Short URL does not exist...')
+  }
+  
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  
+  if (urlsForUser(req.cookies["userID"],req.params.shortURL)){
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
+  } else {
+    res.status(400).send("You do not have permission to delete this URL")
+  }
+
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], users: users, userID: req.cookies["userID"]};
-  
+  if (urlsForUser(req.cookies["userID"],req.params.shortURL)){
+  urlDatabase[req.params.shortURL] = {longURL: req.body.newURL, userID: req.cookies["userID"]}
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, users: users, userID: req.cookies["userID"]};
   res.render("urls_show",templateVars)
+  } else {
+    res.status(400).send("You do not have permission to edit this URL")
+  }
 });
 
 app.post("/urls/:shortURL/newURL", (req, res) => {
-  const longURL = req.body.newURL;
-  urlDatabase[req.params.shortURL] = longURL;
+
+    const longURL = req.body.newURL;
+    urlDatabase[req.params.shortURL] = {longURL: longURL, userID: req.cookies["userID"]};
+    res.redirect("/urls");
   
-  
-  res.render("/urls")
 });
 
 app.post("/login", (req, res) => {
